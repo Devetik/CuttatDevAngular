1065e2bfbfe58727a6362d66af8f7d356149296f
Cuttat-Dev <h3lve@laposte.net>
2022-03-24 21:57:21 +0100

    Première version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProjetsComponent } from './projets/projets.component';
 
-const routes: Routes = [
-  { path: "", component: HomeComponent},
-  { path: "", component: ProjetsComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
